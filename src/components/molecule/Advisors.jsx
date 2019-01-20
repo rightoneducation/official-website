1069e7f5543c8617be9a64c7bc2be003773c08b6
dazyang <dazyang@protonmail.com>
2019-01-20 13:49:22 +1300

    removed console.log / update package.json

--- src/components/molecule/Advisors.jsx
+++ src/components/molecule/Advisors.jsx
@@ -5,7 +5,6 @@ import hash from 'images/hash_icon.svg'
 import Advisor from '../atom/Advisor'
 
 function Advisors(props) {
-  console.log(props)
   return (
     <div className='card-wrapper advisor'>
       <h2><img src={hash} className='about-icons' alt='product feature' />Advisors</h2>

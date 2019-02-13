54ab141b47b73653badad26853902582af2bca55
dazyang <dazyang@protonmail.com>
2019-02-13 19:36:08 +1300

    update ogp property

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0606b53c8c72d6a278ca907ddc868e2.js"
+  "/precache-manifest.86f7127742c8bd28ccb8d31f32d48cf2.js"
 );
 
 workbox.clientsClaim();

5eaaa63bbfd65d077e1e421623ca70e4251da9c7
dazyang <dazyang@protonmail.com>
2019-11-02 15:07:47 +0800

    extra profile photos and updated meta

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da87db3e9fe0fb38a5ffaba0169bef0b.js"
+  "/precache-manifest.1f389ad8355a19d8f7f05bd2f72a7075.js"
 );
 
 workbox.clientsClaim();

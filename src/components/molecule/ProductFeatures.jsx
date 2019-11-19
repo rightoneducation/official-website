7bbe2236e10fca64d73d13e05c683af8b2e93ece
dazyang <daz.yang@codeformosan.com>
2019-11-19 22:55:00 +1300

    added border

--- src/components/molecule/ProductFeatures.jsx
+++ src/components/molecule/ProductFeatures.jsx
@@ -5,8 +5,8 @@ import SingleFeature from '../atom/SingleFeature'
 function ProductFeatures (props) {
     return (
       <section id='features'>
-        <div>
-          <h1 id='section-heading' style={{textAlign:'center', color:'white', fontSize:'2em'}}>Product Features</h1>
+        <div className='section-heading half-circle'>
+          <h1 id='section-heading' style={{textAlign:'center', color:'white', fontSize:'3em'}}>Product Features</h1>
         </div>
         <div className='wrapper'>
           {props.features.features.map((feature, key) => { return <SingleFeature key={key} feature={feature} />

2c182cd3788da33e9627dd628c2590796e05dec1
dazyang <daz.yang@codeformosan.com>
2019-12-02 13:20:06 +1300

    updated our values photo

--- src/components/molecule/OurValue.jsx
+++ src/components/molecule/OurValue.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 
 import teamIcon from '../../images/team-icon.svg'
-
+import placeholder from 'images/team-photo.jpg'
 
 function OurValue () {
     return (
-        <section className='top-section wrapper' id='our-values'>
-        <div className='grid-d-3 grid-tl-12'>
-        <img src={teamIcon} alt='right-on-education-team-icon' width='100%' />
+        <section className='top-section' id='our-values'>
+        <div className='banner-image'>
+          <img  src={placeholder} alt='right-on-education-vision' width='100%' align='top' />
         </div>
-        <div className='grid-d-8 flex-box'>
-            <div style={{alignSelf:'center'}}>
+        <div className='page-intro'>
             <h1>Our Values</h1>
-            <div className='page-intro-content'>
+          <div className='page-intro-content'>
             <p style={{ lineHeight: '1.6em'}}>
                 We’re a team that sometimes falls down, always gets back up, and never stops having fun.<br/>
-                    <b>Trust and open communication:</b> seeking to understand, then to be understood<br/>
-                    <b>Integrity:</b> doing the right thing<br />
-                    <b>Compassion:</b> lending a hand to others, giving back, expressing gratitude<br />
-                    <b>Humility:</b> working together on something bigger than ourselves<br />
-                    <b>Beginner’s mindset:</b> going into things with an open mind, always learning and making new mistakes<br />
-                    <b>Fun:</b> having fun along the way!
+                <b>Trust and open communication:</b> seeking to understand, then to be understood<br/>
+                <b>Integrity:</b> doing the right thing<br />
+                <b>Compassion:</b> lending a hand to others, giving back, expressing gratitude<br />
+                <b>Humility:</b> working together on something bigger than ourselves<br />
+                <b>Beginner’s mindset:</b> going into things with an open mind, always learning and making new mistakes<br />
+                <b>Fun:</b> having fun along the way!
             </p>
-            </div>
-            </div>
+          </div>
         </div>
       </section>
     )

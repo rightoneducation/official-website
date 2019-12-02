ee6952ac8f1f1ac2fe95fc281f2b11220c50deee
dazyang <daz.yang@codeformosan.com>
2019-12-02 16:12:20 +1300

    removed unwanted code

--- src/components/atom/CultureOfError.jsx
+++ src/components/atom/CultureOfError.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {VelocityComponent, VelocityTransitionGroup } from 'velocity-react'
 
 import ExpandInfo from './ExpandInfo'
 
@@ -28,37 +27,3 @@ function CultureOfError (props) {
 }
 
 export default CultureOfError;
-
-
-
-
-
-
-
-
-// <div 
-//                 id='culture-of-error' 
-//                 className='flex-box'>
-//                 <div className='grid-8'>
-//                     <img src={image} width='100%' align="middle"/>
-//                 </div>
-//                 {/* <button id='expand' style={{position: 'absolute'}}>
-//                     <img src={expandBtn}/>
-//                 </button> */}
-//                 {/* <VelocityTransitionGroup 
-//                     enter={{animation: 'slideDown', duration: {duration}}} 
-//                     leave={{animation: 'slideUp',duration: {duration}}}
-//                     > */}
-//                 {/* {expanded ? <ExpandInfo /> :  */}
-//                 <div className='slide-card grid-4'>
-//                     <h2>
-//                     Positive Culture of Error
-//                     </h2>
-//                     <p>
-//                         RightOn! creates a simple way for math teachers to foster a positive culture of error: increasing engagement and building self-confidence through an app-based activity that surfaces mistakes and misconceptions in a safe, fun environment.
-//                     </p>
-//                 {/* <button onClick={handleExpand}>Click</button> */}
-//                 </div>
-//                 {/* </VelocityTransitionGroup> */}
-//                 </div>
-//               {/* </div> */}

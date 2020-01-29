b94f69e1adf005f2c42fe91002dd08334a7d607c
dazyang <dazyang@protonmail.com>
2020-01-29 23:16:16 +0800

    target blank

--- src/components/atom/SingleAdvisor.jsx
+++ src/components/atom/SingleAdvisor.jsx
@@ -4,7 +4,7 @@ function Advisor (props) {
   const {name, photo, profile,title, linkedIn} = props.advisor
   return (
       <div className='grid-d-5 single-advisor-profile'>
-        <a href={linkedIn}><img src={photo} className='profile-photo-advisors' alt='' /></a>
+        <a href={linkedIn} target='_blank'><img src={photo} className='profile-photo-advisors' alt='' /></a>
           <div className='advisor-info'>
             <h2 style={{fontWeight: '300', marginRight: '10px'}}>{name}</h2>
             <h5 className='high-light'>{title}</h5>

90b7a8d6170d57a5d88490b8c59cea5861bdd0d0
dazyang <dazyang@protonmail.com>
2020-06-03 15:06:13 +0800

    updating profile cards

--- src/components/atoms/ProfileFront.jsx
+++ src/components/atoms/ProfileFront.jsx
@@ -1,24 +1,73 @@
 import React from 'react'
 import { makeStyles } from "@material-ui/core/styles";
+import { Typography, Grid } from "@material-ui/core";
+import LinkedIn from "../../images/linkedin-icon.svg";
 
 const useStyles = makeStyles({
   profilePhoto: {
     cursor: "pointer",
-    width: "249px",
-    marginBottom: "-20px"
+    width: "180px",
+    marginLeft: "-15px",
+    marginBottom: "10px",
+    borderTop: "10px solid white",
+    borderLeft: "10px solid white",
+  },
+  jobTitle: {
+    marginTop: "10px",
+    marginBottom: "10px",
+    height: "50px",
+    color: "white",
+    backgroundBlendMode: "multiply",
+    background: "rgba(39, 2, 48, 0.377)",
+    padding: "10px 20px",
   }
 });
 
 function Front (props) {
-    const {photo, handleFlip} = props
+    const {handleFlip} = props
+    const { photo, name, linkedIn, title } = props.profile;
     const styles = useStyles();
     return (
-        <div>
-            <button onMouseOver={handleFlip} onClick={handleFlip}>
-                <img src={photo} width='100%' className={styles.profilePhoto} alt='right-on-education-team-member'/>
-            </button>
+      <div onMouseOver={handleFlip} onClick={handleFlip}>
+        <div style={{display: "flex", justifyContent: "center"}}>
+          <img
+            src={photo}
+            width="100%"
+            className={styles.profilePhoto}
+            alt="right-on-education-team-member"
+          />
         </div>
-    )
+        <Grid container alignItems="center">
+          <Typography
+            variant="h6"
+            style={{
+              fontSize: "18px",
+              fontStyle: "italic",
+              marginBottom: "unset",
+            }}
+          >
+            {name}
+          </Typography>
+          <a target="_blank" href={linkedIn} rel="noopener noreferrer">
+            <img
+              src={LinkedIn}
+              width="20px"
+              height="20px"
+              style={{ margin: "0 0 -2px 6px" }}
+              alt="member-profile-photos"
+            />
+          </a>
+        </Grid>
+        <Grid container alignItems="center" className={styles.jobTitle}>
+          <Typography
+            variant="body2"
+            style={{ fontWeight: "300", lineHeight: "1.2" }}
+          >
+            {title}
+          </Typography>
+        </Grid>
+      </div>
+    );
 }
 
 export default Front;
--- src/components/atoms/SingleMember.jsx
+++ src/components/atoms/SingleMember.jsx
@@ -4,7 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Typography, Grid } from "@material-ui/core";
 import Front from './ProfileFront'
 import Back from './ProfileBack'
-import LinkedIn from '../../images/linkedin-icon.svg'
+
 
 const useStyles = makeStyles({
   profileCard: {
@@ -15,54 +15,29 @@ const useStyles = makeStyles({
   paddingRight: "0em",
   paddingBottom: "0em",
   color: "white",
-  background: "linear-gradient(250deg, rgb(129, 8, 156) 1.99%, #662AAF 50.9%), rgb(112, 35, 129)",
+  background: "linear-gradient(180deg, rgb(129, 8, 156) 1.99%, #662AAF 50.9%), rgb(112, 35, 129)",
   mixBlendMode: "multiply",
-  },
-  jobTitle: {
-    marginTop: "10px",
-    marginBottom: "10px",
-    height: "50px",
-    color: "white",
-    backgroundBlendMode: "multiply",
-    background: "rgba(39, 2, 48, 0.377)",
-    padding: "10px 20px",
   }
-
 })
 
 function SingleMember (props) {
-  const {name, linkedIn, photo, title, favoriteShow, favoriteMistake, isFlipped} = props.profile
+  const { favoriteShow, favoriteMistake, isFlipped} = props.profile
   const { handleFlip } = props  
   const styles = useStyles()
   return (
     <div>
-    <Grid className={styles.profileCard}>
-      <Grid container alignItems="center">
-        <Typography variant="h6" style={{ fontStyle: "italic", marginBottom: "unset" }}>
-          {name}
-        </Typography>
-          <a target="_blank" href={linkedIn} rel="noopener noreferrer">
-            <img
-              src={LinkedIn}
-              width="20px"
-              height="20px"
-              style={{ margin: "0 0 -2px 6px" }}
-              alt="member-profile-photos"
-            />
-          </a>
-      </Grid>
-      <Grid container alignItems="center"  className={styles.jobTitle}>
-        <Typography variant="subtitle2" style={{ fontWeight: "300", lineHeight: "1.2" }}>
-        {title}
-        </Typography>
-      </Grid>
+    <Grid>
       <ReactCardFlip isFlipped={isFlipped}>
-        <Front photo={photo} handleFlip={() => handleFlip()} />
-        <Back
-          favMistake={favoriteMistake}
-          favShow={favoriteShow}
-          handleFlip={() => handleFlip()}
-        />
+        <Grid className={styles.profileCard}>
+          <Front {...props} handleFlip={() => handleFlip()} />
+        </Grid>
+        <Grid className={styles.profileCard}>
+          <Back
+            favMistake={favoriteMistake}
+            favShow={favoriteShow}
+            handleFlip={() => handleFlip()}
+          />
+        </Grid>
       </ReactCardFlip>
     </Grid>
     </div>

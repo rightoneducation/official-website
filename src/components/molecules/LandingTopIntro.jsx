969b0d9b81e79741328107b63acde67e0c5cba5d
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-10-29 15:03:20 -0400

    Ryan's feedback

--- src/components/molecules/LandingTopIntro.jsx
+++ src/components/molecules/LandingTopIntro.jsx
@@ -10,10 +10,10 @@ function LandingTopIntro(props) {
         <Typography variant="h6" className={styles.cardContent}>
           Our mission is to make STEM more engaging for students, 
           increasing both motivation and self-confidence. Not a big
-          fan of math or science? Great,{" "}
+          fan of math or science? Great,
           <b>
             <em>RightOn!</em>
-          </b>{" "}
+          </b>
           is the app for you. Already like STEM? That’s cool too, you’ll like
           it even more!
         </Typography>

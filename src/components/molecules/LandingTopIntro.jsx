bbf5b24be67b0fbcbe3bb6514b3898251e4c6ef7
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-10-29 15:09:11 -0400

    Sinclair's Text-Updates  text-updates (#3)
    * Updated text information for website sections

--- src/components/molecules/LandingTopIntro.jsx
+++ src/components/molecules/LandingTopIntro.jsx
@@ -8,13 +8,14 @@ function LandingTopIntro(props) {
       <CardContent>
         <Typography variant="h4">Mission</Typography>
         <Typography variant="h6" className={styles.cardContent}>
-          Our mission is to make math more engaging for students, increasing
-          both motivation and self-confidence. Not a big fan of math? Great,{" "}
+          Our mission is to make STEM more engaging for students, 
+          increasing both motivation and self-confidence. Not a big
+          fan of math or science? Great,
           <b>
             <em>RightOn!</em>
-          </b>{" "}
-          is the app for you. Already like math? That’s cool too, you’ll like it
-          even more!
+          </b>
+          is the app for you. Already like STEM? That’s cool too, you’ll like
+          it even more!
         </Typography>
       </CardContent>
     </Card>

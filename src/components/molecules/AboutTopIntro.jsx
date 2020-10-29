2c5ecab6ef453d635c78dc094221a0ffc1ffc9eb
Eric Nguyen <53154237+EricNguyen-B@users.noreply.github.com>
2020-10-29 15:03:20 -0400

    Ryan's feedback

--- src/components/molecules/AboutTopIntro.jsx
+++ src/components/molecules/AboutTopIntro.jsx
@@ -8,10 +8,10 @@ function AboutTopIntro(props) {
         <CardContent>
           <Typography variant="h4">Our Vision</Typography>
           <Typography variant="h6" className={styles.cardContent}>
-            In the future, we envision{" "}
+            In the future, we envision
             <b>
               <em>RightOn!</em>
-            </b>{" "}
+            </b>
             to become a multi-channel learning platform (spanning mobile apps
             and livestream video), one that eventually helps connect students of
             all ages and backgrounds to learn new subjects – from math today to

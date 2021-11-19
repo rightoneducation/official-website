6e76c9e5deacd3f066ec6d8132cb704e4d201f28
zach2025 <zachthomas2025@gmail.com>
2021-11-19 13:38:31 -0500

    error analysis activties section

--- src/components/pages/ResourcePage.jsx
+++ src/components/pages/ResourcePage.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import PositiveCultureOfErrorSection from 'components/organisms/PositiveCultureOfErrorSection';
+import ErrorAnalysisActivitiesSection from '../organisms/ErrorAnalysisActivitiesSection';
 
 function ResourcePage(props) {
     const { styles } = props
         return(
             <div>
                 <PositiveCultureOfErrorSection styles={styles}/>
+                <ErrorAnalysisActivitiesSection/>
             </div>
         )
-}
+    }
 
 export default ResourcePage;

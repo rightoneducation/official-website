827f6167f11190eb7c5faaf02e046c7dae1eae47
Jared <jmj379@drexel.edu>
2021-11-20 13:55:10 -0500

    Add CRMT and GameShow Sections to Resources Page

--- src/components/pages/ResourcePage.jsx
+++ src/components/pages/ResourcePage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PositiveCultureOfErrorSection from 'components/organisms/PositiveCultureOfErrorSection';
 import ErrorAnalysisActivitiesSection from '../organisms/ErrorAnalysisActivitiesSection';
+import CRMT from '../organisms/CRMT';
+import RightOnGameShow from '../organisms/RightOnGameShow';
 
 function ResourcePage(props) {
     const { styles } = props
@@ -8,6 +10,8 @@ function ResourcePage(props) {
             <div>
                 <PositiveCultureOfErrorSection styles={styles}/>
                 <ErrorAnalysisActivitiesSection/>
+                <CRMT />
+                <RightOnGameShow />
             </div>
         )
     }

c5c76febad3bff0ff3d3e22b8bbfaa28cff0738e
Jared <jmj379@drexel.edu>
2021-11-20 13:55:10 -0500

    Add CRMT and GameShow Sections to Resources Page

---
+++ src/components/atoms/GameShowVideo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+export default function GameShowVideo() {
+  return (
+      <Grid xs={12} sm={12} md={8}>
+        <iframe
+          width="100%"
+          height="550"
+          src="https://www.youtube.com/embed/_dNfmPa6CRo"
+          title="RightOn! Gameshow"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Grid>
+  );
+}

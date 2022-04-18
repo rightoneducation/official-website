20c95018a06dd88faf8ed80e78cd242927be0a86
josephinaim <jhi002247@gmail.com>
2022-04-18 16:51:25 -0400

    add meet the team banner and title image

--- src/components/organisms/TeamMembers.jsx
+++ src/components/organisms/TeamMembers.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Box, Typography, Grid } from "@material-ui/core";
 import ProfileMembers from '../molecules/ProfileMembers'
-
+import placeholder from '../../images/team-title.png'
 function TeamMembers(props) {
   return (
     <Box component="section">
-      <Typography
+      {/* <Typography
         variant="h4"
         style={{ marginBottom: "1.5em", fontWeight: "500", textAlign: "center" }}
       >
         Meet the Team
-      </Typography>
+      </Typography> */}
+      <img
+        src={placeholder}
+        alt="right-on-education-team-title"
+        width="100%"
+        align="top"
+      />
       <Grid container justify="center">
         <ProfileMembers {...props}/>
       </Grid>

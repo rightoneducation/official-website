5d174ed3bb677116145d314ebff3717f58e9d402
josephinaim <jhi002247@gmail.com>
2022-05-13 10:59:09 -0400

    thumbnail fixed

--- src/components/atoms/RightOnVideo.jsx
+++ src/components/atoms/RightOnVideo.jsx
@@ -7,7 +7,7 @@ function RightOnVideo() {
         <iframe
           width="100%"
           height="420"
-          src="https://youtube.com/embed/k_GEcWXJABM"
+          src="https://www.youtube.com/embed/k_GEcWXJABM"
           title="RightOn! Beta overview"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"

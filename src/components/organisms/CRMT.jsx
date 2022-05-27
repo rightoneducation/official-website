b6af76149da021067afa822072e6f60f66505fd4
josephinaim <jhi002247@gmail.com>
2022-05-27 14:06:11 -0400

    triangles added to crmt section

--- src/components/organisms/CRMT.jsx
+++ src/components/organisms/CRMT.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Box, Grid, Typography } from "@material-ui/core";
 import CRMTimage from '../../images/crmt.png';
+import triangleUp from "../../images/background-triangle-up.png"
+import triangleDown from "../../images/background-triangle.png"
 
 export default function CRMT(props) {
     // const { styles } = props;
     return (
-        <Box component="section">
+        <Box >
+            <img
+                src={triangleUp}
+                width="100%"
+                style={{marginBottom:"-250px"}}
+            />
             <Grid container style={{ margin: "100px" }}>
+                {/* <Grid item>
+                    <img
+                        src={triangleUp}
+                        width="100%"
+                        
+                    />
+                </Grid> */}
                 <Grid item style={{ align: "center" }}>
                     <h1 className='crmt-header'>
                         Culturally Responsive <br /> Mathematics Teaching
@@ -28,6 +42,11 @@ export default function CRMT(props) {
                     <img src={CRMTimage} alt="CRMT-Diagram" style={{ justifyContent: "right" }} />
                 </Grid>
             </Grid>
+            <img
+                src={triangleDown}
+                width="100%"
+                style={{marginTop:"-550px"}}
+            />
             {/* <img src={CRMTimage} alt="CRMT-Diagram" /> */}
         </Box>
     );

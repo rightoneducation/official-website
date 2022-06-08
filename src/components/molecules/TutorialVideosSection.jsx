834465818305059d07bc372b67420e4769a300d3
Lucah Endicott <lucahdavid88@gmail.com>
2022-06-08 12:20:02 -0700

    add mapping for video links

--- src/components/molecules/TutorialVideosSection.jsx
+++ src/components/molecules/TutorialVideosSection.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 import { Box, Grid, Typography } from "@material-ui/core";
 
 function TutorialVideos({ videoSrc }) {
-  console.log({videoSrc})
+  const videoLink = videoSrc.map((video, index) => (
+    <p key={index}> {video.title} </p>
+  ))
+  
   return (
     <Box components="section">
       <div className="tutorial-video-panel">
         <h4 className="tutorial-videos-header-text">Tutorial Videos</h4>
         <div className="tutorial-videos-panel">
-          <p className="tutorial-videos-text">videoSrc<br></br></p>
-          <p className="tutorial-videos-text">Host and Display Overview<br></br></p>
-          <p className="tutorial-videos-text">Basic vs Advanced<br></br></p>
+          <p className="tutorial-videos-text">{videoLink}<br></br></p>
         </div>
       </div>
     </Box>

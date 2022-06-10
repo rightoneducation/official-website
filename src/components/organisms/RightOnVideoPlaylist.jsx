c448f9773d601a442eb7e1954319166f8eb6cbb0
Alexandra Kushnirsky <ask256@cornell.edu>
2022-06-10 09:38:49 -0400

    Moved components to one primary file, continuing to add functionality to playlist.

--- src/components/organisms/RightOnVideoPlaylist.jsx
+++ src/components/organisms/RightOnVideoPlaylist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Grid } from "@material-ui/core";
 import GameShowHeading from "../molecules/GameShowHeading";
 import OverviewVideo from "../atoms/OverviewVideo"
@@ -8,26 +8,48 @@ import triangle from "../../images/background-triangle.png"
 import AboutBannerImage from "../molecules/AboutBannerImage";
 import TutorialVideos from "../molecules/TutorialVideosSection";
 
-export default function RightOnVideoPlaylist(props) {
+//take id of clicked video link - set src and title based off current/clicked id
 
-    console.log(props)
-    console.log(props.videoSrc[0].src)
+export default function RightOnVideoPlaylist(props) {
+    const [currentVideo, setCurrentVideo] = useState()
+
+    //mapping over tutorial videos objects from features.db - returning list of video titles
+    const videoLink = props.videoSrc.map((video) => (
+        <p key={video.id}> {video.title}</p>
+
+    ))
+
     return (
         <Box >
             <AboutBannerImage />
             <Grid container justify="space-evenly" alignItems="top" spacing={1}>
-                <Grid>
-                    <TutorialVideos {...props} />
+                <Grid >
+                    <Box components="section">
+                        <div className="tutorial-video-panel">
+                            <h4 className="tutorial-videos-header-text">Tutorial Videos</h4>
+                            <div className="tutorial-videos-panel">
+                                <button onClick={() => console.log("CLICKED:", props.video.id)} className="tutorial-videos-text">{videoLink}<br></br></button>
+                            </div>
+                        </div>
+                    </Box>
+                    {/*<TutorialVideos {...props} /> */}
                 </Grid>
                 <Grid>
-                    <OverviewVideoHeading />
+                    <Grid container justify="left">z
+                        <Grid item>
+                            <h4 className="overview-video-heading">
+                                {props.videoSrc[0].title}
+                            </h4>
+                            <br />
+                        </Grid>
+                    </Grid>
                     <Grid xs={12} sm={12} md={8} style={{ marginBottom: "70px" }}>
                         <div>
                             <iframe
                                 width="860"
                                 height="483"
                                 src={props.videoSrc[0].src}
-                                title="RighOn! Overview"
+                                title={props.videoSrc[0].title}
                                 frameborder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowfullscreen

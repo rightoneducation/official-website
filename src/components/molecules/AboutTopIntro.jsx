4c1812f798133de9daafbdfdecab501c21cfc4ec
Alexandra Kushnirsky <ask256@cornell.edu>
2022-06-20 15:15:24 -0400

    Values card now a component

--- src/components/molecules/AboutTopIntro.jsx
+++ src/components/molecules/AboutTopIntro.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { Grid, GridDirection } from "@material-ui/core";
-import placeholder from '../../images/vision-desktop.png'
+import placeholder from '../../images/vision-desktop.png';
+import vision from '../../images/vision.png';
 
 function AboutTopIntro(props) {
   const { styles } = props;
   return (
     <Grid>
-    <img
+      <div className="vision-card">
+        <h1 className="mission-and-vision-header">Vision</h1>
+        <div className="mission-and-vision-underline"></div>
+        <p className="mission-and-vision-text">Unlocking STEM potential in all youth.</p>
+
+
+      </div>
+      <img
+        src={vision}
+        alt="right-on-education-vision"
+        textAlign="center"
+        width="380px" />
+
+
+
+      {/*<img
     src={placeholder}
     alt="right-on-education-vision"
     width="85%"
     max-width="756.4px"
-  />
-  </Grid>
+    />*/}
+    </Grid>
   );
 }
 
--- src/components/molecules/AboutTopMission.jsx
+++ src/components/molecules/AboutTopMission.jsx
@@ -4,30 +4,31 @@ import placeholder from '../../images/mission-desktop.png'
 import monster from '../../images/mission.png'
 
 function AboutTopMission(props) {
+
   return (
+
     <Grid>
+
       <div className="mission-card">
         <h1 className="mission-and-vision-header">Mission</h1>
         <div className="mission-and-vision-underline"></div>
 
-        <div marginTop="20px">
-          <div className="mission-and-vision-text-top" >
-            <div>
-              Creating classroom environments<br></br>that
-              <strong> embrace mistakes and</strong><br></br></div>
-          </div>
 
+        <div className="mission-and-vision-text" >
+
+          Creating classroom environments<br></br>that
+          <strong> embrace mistakes and</strong><br></br>
         </div>
 
-
-
-
-        <div className="mission-and-vision-text-bottom">
-          {/*<img src={monster}
+        <div>
+          <img src={monster}
             alt="right-on-education-mission"
-  width="157px" /> */}
-          <div>
-            <strong>misconceptions  </strong><br></br>by turning them into<br></br>
+            width="230px" float="left" />
+
+          <div className="mission-and-vision-text">
+
+
+            <strong>misconceptions </strong><br></br>by turning them into<br></br>
             learning opportunities.</div>
         </div>
 

0fc115a47fe9380a7421c43e808d2836a3aa97f5
Alexandra Kushnirsky <ask256@cornell.edu>
2022-07-01 14:14:23 -0400

    Made advisor card images into card components

--- src/components/atoms/SingleAdvisor.jsx
+++ src/components/atoms/SingleAdvisor.jsx
@@ -1,31 +1,62 @@
 import React from 'react'
-import { Grid } from "@material-ui/core";
+import { Grid, Card, CardContent, LinearProgress } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import icon from "../../images/linkedin.png"
 
 const useStyles = makeStyles({
-  advisorPhoto: {
+  /*advisorPhoto: {
     width: "350px",
     height: "625px",
     marginTop: "28px"
-  },
+  },*/
 
 });
-function Advisor (props) {
-  const {name, photo, profile,title, linkedIn} = props.advisor
+function Advisor(props) {
+  const { name, photo, profile, title, linkedIn, imBackground, txtBackground } = props.advisor
   const styles = useStyles();
   return (
     <Grid container>
       {/* <Grid container alignItems="center"> */}
-        <Grid item xs={12}>
-        <a href={linkedIn} target='_blank' rel="noopener noreferrer">
-          <img 
-            src={photo} 
-            className={styles.advisorPhoto} 
-            alt='' 
-            />
-        </a>
-        </Grid>
-        {/* <Grid item>
+
+      <Card style={{
+        width: "335px", height: "635px", alignItems: "center",
+        borderRadius: "30px", boxShadow: "10px 10px 10px rgba(0, 0, 0, 0.25)",
+        background: txtBackground, margin: "20px", marginTop: "30px"
+      }}>
+        <CardContent style={{ background: imBackground, justifyContent: "center", padding: "0px" }}>
+
+          <img
+            width="250px"
+            style={{
+              clipPath: "circle()", marginTop: "24px", marginLeft: "42px",
+              marginRight: "42px", marginBottom: "0px"
+            }}
+            src={photo}
+            className={styles.advisorPhoto}
+            alt=''
+          />
+
+        </CardContent>
+        <CardContent style={{ paddingLeft: "26px", paddingRight: "26px", paddingTop: "18px" }}>
+          <div className="advisor-header">
+            {name}
+            <a href={linkedIn}>
+              <img
+                style={{ float: "right" }}
+                width="30px"
+                src={icon}
+              /></a>
+            <div style={{ fontSize: "14px", fontWeight: "400", lineHeight: "30px" }}>
+              {title}
+            </div>
+          </div>
+          <div className="advisor-description" style={{ marginTop: "10px" }}>
+            {profile}
+          </div>
+        </CardContent>
+
+      </Card>
+      {/* <Grid item>
         <h3 style={{fontWeight: '300', marginRight: '10px'}}>{name}</h3>
           <h5 className={styles.jobTitle}>{title}</h5>
         </Grid>
@@ -33,7 +64,7 @@ function Advisor (props) {
       <Grid>
         <p style={{fontSize: '.9rem'}}>{profile}</p>
       </Grid> */}
-    </Grid>
+    </Grid >
   )
 }
 

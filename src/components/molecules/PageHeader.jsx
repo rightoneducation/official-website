29348027fdee078dc31a6d8d1f2abdc94ff99247
josephinaim <jhi002247@gmail.com>
2022-07-07 11:49:49 -0400

    new logo and page hyperlinks now clickable

--- src/components/molecules/PageHeader.jsx
+++ src/components/molecules/PageHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Grid, Typography } from "@material-ui/core";
 import logo from '../../images/right-on-logo-circle.png'
-import triangle from '../../images/triangle.png'
+import mobileLogo from '../../images/mobile-logo.png'
 
 import MainNav from './MainNav'
 
@@ -12,15 +12,7 @@ const useStyles = makeStyles((theme) => ({
       justifyContent: "center",
       alignItems: "center"
     },
-  },
-  // subTitle: {
-  //   textAlign: "right",
-  //   fontStyle: "italic",
-  //   [theme.breakpoints.down("xs")]: {
-  //     fontSize: "1.1em",
-  //     textAlign: "center"
-  //   },
-  // },
+  }
 }));
 
 function PageHeader() {
@@ -28,10 +20,6 @@ function PageHeader() {
   return (
     <Box component="header" className="page-header">
       <div className="header-elements">
-        
-        {/* <div id="header-element-1"></div> */}
-        {/* <div id="header-element-2"></div> */}
-        {/* <div id="header-element-3"></div> */}
       </div>
       <Grid
         container
@@ -43,6 +31,7 @@ function PageHeader() {
         <Grid item sm={4} md={5}>
           <a href="/">
             <img src={logo} alt="right on education logo" className='logo' />
+            <img src={mobileLogo} alt="right on education logo" className='mobile-logo'/>
           </a>
         </Grid>
         <Grid
